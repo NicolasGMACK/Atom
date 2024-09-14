c1e10689fb7447643bca5c5717d872169ac855ea
Nicolas <nicolasgmack@gmail.com>
2024-09-14 13:23:24 -0300

    mudança filtro intervalo

--- view/js/filtroIntervalo.js
+++ view/js/filtroIntervalo.js
@@ -1,3 +1,20 @@
 document.getElementById('openIntervalo').addEventListener('click', function() {
-    document.querySelector('.intervalo').style.display = 'flex';
-})
+    var intervalo = document.querySelector('.intervalo');
+    var openIntervalo = document.getElementById('openIntervalo');
+    
+    // Obter o valor de display calculado
+    var currentDisplay = window.getComputedStyle(intervalo).display;
+    
+    if (currentDisplay === 'flex') {
+        intervalo.style.display = 'none';
+        openIntervalo.classList.remove('open');
+        openIntervalo.classList.add('closed');
+    } else {
+        intervalo.style.display = 'flex';
+        openIntervalo.classList.remove('closed');
+        openIntervalo.classList.add('open');
+    }
+});
+
+// Inicialmente, definir a classe closed para o texto
+document.getElementById('openIntervalo').classList.add('closed');
