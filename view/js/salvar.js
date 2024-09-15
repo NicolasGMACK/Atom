e05d2546902176279738530d54982ff49d0295e7
Nicolas <nicolasgmack@gmail.com>
2024-09-15 16:15:28 -0300

    notificação salvo

--- view/js/salvar.js
+++ view/js/salvar.js
@@ -1,11 +1,24 @@
 const saveButton = document.getElementById('Salvar');
+const notification = document.getElementById('notification');
+const notificationTitle = document.getElementById('notificationTitle');
+const notificationText = document.getElementById('notificationText');
 
-    saveButton.addEventListener('click', function() {
-        if (saveButton.classList.contains('saved')) {
-            saveButton.textContent = 'Salvar';
-            saveButton.classList.remove('saved');
-        } else {
-            saveButton.textContent = 'Em biblioteca';
-            saveButton.classList.add('saved');
-        }
-    });
+saveButton.addEventListener('click', function() {
+    if (saveButton.classList.contains('saved')) {
+        saveButton.textContent = 'Salvar';
+        saveButton.classList.remove('saved');
+    } else {
+        saveButton.textContent = 'Em biblioteca';
+        saveButton.classList.add('saved');
+
+        // Show notification with the title and text
+        notificationTitle.textContent = `Arquivo salvo com sucesso!`;
+        notificationText.textContent = `Você pode encontrar o arquivo no seu perfil.`;
+        notification.style.display = 'block';
+
+        // Hide the notification after 3 seconds
+        setTimeout(function() {
+            notification.style.display = 'none';
+        }, 3000);
+    }
+});
